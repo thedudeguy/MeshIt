651690e236c9c1533fe3fd2601f658f6d0c63d57
Chris Churchwell <chrisrulesall@gmail.com>
2012-08-10 04:11:10 -0500

    First Commit

---
+++ src/com/chrischurchwell/meshit/ModelUV.java
@@ -0,0 +1,54 @@
+/**
+ * This file is part of MeshIt
+ *
+ * Copyright (C) 2012  Chris Churchwell
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.chrischurchwell.meshit;
+
+public class ModelUV {
+
+	private float u;
+	private float v;
+	
+	public ModelUV(float u, float v) {
+		this.setU(u);
+		this.setV(v);
+	}
+
+	public float getU() {
+		return u;
+	}
+
+	public void setU(float u) {
+		this.u = u;
+	}
+
+	public float getV() {
+		return v;
+	}
+
+	public void setV(float v) {
+		this.v = v;
+	}
+	
+	public int getTx(int textureWidth) {
+		return Math.round(u * (float)textureWidth);
+	}
+	
+	public int getTy(int textureHeight) {
+		return Math.round(v * (float)textureHeight);
+	}
+}
